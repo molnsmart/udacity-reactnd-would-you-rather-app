63f8c5427ac2dc92a2618f18813242898ca15f92
Tommy Vadman <tom@molnsmart.se>
2021-10-21 20:07:02 +0200

    cleanup QuestionList

--- src/components/Question/QuestionList.js
+++ src/components/Question/QuestionList.js
@@ -6,82 +6,23 @@ import { formatUserObjectList, getUser } from '../../utils/helpers'
 import { withRouter } from "react-router";
 import Question from './Question';
 class QuestionList extends Component {
-  state = {
-    questionListWithAnswers: [],
-    questionListWithOoutAnswers: [],
-    loaded: false,
-  }
-  componentDidMount() {
-    this.updateQuestionLists();
-  }
-  hideQuestionListWithAnswers() {
-
-  }
-  updateQuestionLists() {
-    let questionList = formatUserObjectList(this.props.questions)
-    let questionsWithAnswers = questionList.filter(x => (x.optionOne.votes.length > 0 || x.optionTwo.votes.length > 0))
-    let questionsWithOutAnswers = questionList.filter(x => (x.optionOne.votes.length === 0 && x.optionTwo.votes.length === 0))
-    this.setState(
-      {
-        questionListWithAnswers: questionsWithAnswers,
-        questionListWithOutAnswers: questionsWithOutAnswers,
-        loaded: true
-      }
-    )
-  }
   render() {
-    if (this.state.loaded) {
-      return (
-        <div>
-          <div class="row mt-3">
-            <div class="col-4"></div>
-            <div class="col-4 mt-0">
-              <Tabs>
-                <TabList>
-                  <Tab><h5 className="text-center">Unanswered Questions</h5></Tab>
-                  <Tab><h5 className="text-center">Answered Questions</h5></Tab>
-                </TabList>
-                <div className="col-12 border">
-                  <TabPanel>
-                    <div class="row mt-5">
-                      {/* <div class="col-12">
-                        {
-                          this.state.questionListWithAnswers.map(question => {
-                            let user = getUser(this.props.users, question.author)
-                            return (
-                              <Question Question={question} Author={user}></Question>
-                            )
-                          })
-                        }
-                      </div> */}
-                      <div class="col-12">
-                        {
-                          formatUserObjectList(this.props.questions).filter(x => (x.optionOne.votes.length === 0 && x.optionTwo.votes.length === 0)).map(
-                            question => {
-                              let user = getUser(this.props.users, question.author)
-                              return (
-                                <Question Question={question} Author={user}></Question>
-                              )
-                            }
-                          )
-                        }
-                      </div>
-                    </div>
-                  </TabPanel>
-                  <TabPanel>
-                    <div class="row mt-5 mb-3">
-                      {/* <div className="col-12">
-                        {
-                          this.state.questionListWithOutAnswers.map(question => {
-                            let user = getUser(this.props.users, question.author)
-                            return (
-                              <Question Question={question} Author={user}></Question>
-                            )
-                          })
-                        }
-                      </div> */}
+    return (
+      <div>
+        <div class="row mt-3">
+          <div class="col-4"></div>
+          <div class="col-4 mt-0">
+            <Tabs>
+              <TabList>
+                <Tab><h5 className="text-center">Unanswered Questions</h5></Tab>
+                <Tab><h5 className="text-center">Answered Questions</h5></Tab>
+              </TabList>
+              <div className="col-12 border">
+                <TabPanel>
+                  <div class="row mt-5">
+                    <div class="col-12">
                       {
-                        formatUserObjectList(this.props.questions).filter(x => (x.optionOne.votes.length > 0 || x.optionTwo.votes.length > 0)).map(
+                        formatUserObjectList(this.props.questions).filter(x => (x.optionOne.votes.length === 0 && x.optionTwo.votes.length === 0)).map(
                           question => {
                             let user = getUser(this.props.users, question.author)
                             return (
@@ -91,19 +32,30 @@ class QuestionList extends Component {
                         )
                       }
                     </div>
-                  </TabPanel>
-                </div>
+                  </div>
+                </TabPanel>
+                <TabPanel>
+                  <div class="row mt-5 mb-3">
+                    {
+                      formatUserObjectList(this.props.questions).filter(x => (x.optionOne.votes.length > 0 || x.optionTwo.votes.length > 0)).map(
+                        question => {
+                          let user = getUser(this.props.users, question.author)
+                          return (
+                            <Question Question={question} Author={user}></Question>
+                          )
+                        }
+                      )
+                    }
+                  </div>
+                </TabPanel>
+              </div>
 
-              </Tabs>
-            </div>
+            </Tabs>
           </div>
-          <div class="col-4"></div>
         </div>
-      )
-    } else {
-      return null
-    }
-
+        <div class="col-4"></div>
+      </div>
+    )
   }
 
 }
